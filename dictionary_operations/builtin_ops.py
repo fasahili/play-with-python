cb1e9bb07c4fc39c2af1dd4c0c2aa87c747956fc
Faris Al-Sahili <farisasahili@gmail.com>
2025-04-09 01:57:52 +0300

    Add builtin_ops.py

---
+++ dictionary_operations/builtin_ops.py
@@ -0,0 +1,7 @@
+my_dict = {'a': 1, 'b': 2}
+my_list = [1, 2, 3]
+my_tuple = (4, 5, 6)
+
+print("Length of dict:", len(my_dict))
+print("Max of list:", max(my_list))
+print("Sum of tuple:", sum(my_tuple))
